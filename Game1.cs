1441b1f5f7103ea059c1668773813b9abda42b10
362568651 <362568651@2260I-L29-003>
2026-05-19 13:08:17 -0400

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace final_project__race_cars
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        Texture2D menuBackground;
+        enum Screen {Menu, TrackSelect, Race }
+        Screen screen;
+        MouseState mouseState;
+        Rectangle window;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            window = new Rectangle(0, 0, 800, 480);
+            
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.ApplyChanges();
+            screen = Screen.Menu;
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            menuBackground = Content.Load<Texture2D>("menu-screen");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            mouseState = Mouse.GetState();
+            if(screen == Screen.Menu)
+            {
+                if(mouseState.LeftButton == ButtonState.Pressed)
+                {
+                    int mouseX = mouseState.X;
+                    int mouseY = mouseState.Y;
+                }
+
+            }
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+            if(screen == Screen.Menu)
+            {
+                _spriteBatch.Draw(menuBackground, window, Color.White);
+
+            }
+            _spriteBatch.End();
+
+
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new final_project__race_cars.Game1();
+game.Run();
